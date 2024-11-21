c84e04ffaea1d3f8b461f532bab6e7ab30f9ea1b
Baldasar <lucaspaquelin@gmail.com>
2024-11-21 10:09:53 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -23,73 +23,73 @@ ChartJS.register(
 );
 
 export default function App() {
-  const [coords, setCoords] = useState([{ x: 0, y: 0 }]);
-  const [labelType, setLabelType] = useState("Nenhum gráfico formado");
-  const [law, setLaw] = useState("");
-  const [data, setData] = useState([]);
+  const [coordenadas, setCoordenadas] = useState([{ x: 0, y: 0 }]);
+  const [tipoRotulo, setTipoRotulo] = useState("Nenhum gráfico formado");
+  const [lei, setLei] = useState("");
+  const [dados, setDados] = useState([]);
 
-  function bestFitModel(points) {
-    const linear = regression.linear(points);
-    const exponential = regression.exponential(points);
-    const logarithmic = regression.logarithmic(points);
-    const power = regression.power(points);
-    const polynomial = regression.polynomial(points, { order: 2 });
+  function modeloMelhorAjuste(pontos) {
+    const linear = regression.linear(pontos);
+    const exponencial = regression.exponential(pontos);
+    const logaritmica = regression.logarithmic(pontos);
+    const potencia = regression.power(pontos);
+    const polinomial = regression.polynomial(pontos, { order: 2 });
 
-    const models = [
-      { name: "Linear", model: linear },
-      { name: "Exponencial", model: exponential },
-      { name: "Logarítmica", model: logarithmic },
-      { name: "Potência", model: power },
-      { name: "Polinomial", model: polynomial },
+    const modelos = [
+      { nome: "Linear", modelo: linear },
+      { nome: "Exponencial", modelo: exponencial },
+      { nome: "Logarítmica", modelo: logaritmica },
+      { nome: "Potência", modelo: potencia },
+      { nome: "Polinomial", modelo: polinomial },
     ];
 
-    let bestModel = models[0];
-    models.forEach((model) => {
-      if (model.model.r2 > bestModel.model.r2) {
-        bestModel = model;
+    let melhorModelo = modelos[0];
+    modelos.forEach((modelo) => {
+      if (modelo.modelo.r2 > melhorModelo.modelo.r2) {
+        melhorModelo = modelo;
       }
     });
 
-    setLaw(bestModel.model.string);
-    return bestModel;
+    setLei(melhorModelo.modelo.string);
+    return melhorModelo;
   }
 
-  function handleVerify() {
-    const points = coords.map((coord) => [coord.x, coord.y]);
-    const bestModel = bestFitModel(points);
-    setLabelType(bestModel.name);
+  function handleVerificar() {
+    const pontos = coordenadas.map((coord) => [coord.x, coord.y]);
+    const melhorModelo = modeloMelhorAjuste(pontos);
+    setTipoRotulo(melhorModelo.nome);
 
-    const modelData = bestModel.model.points.map((point) => ({
-      x: point[0],
-      y: point[1],
+    const dadosModelo = melhorModelo.modelo.points.map((ponto) => ({
+      x: ponto[0],
+      y: ponto[1],
     }));
-    setData(modelData);
+    setDados(dadosModelo);
   }
 
   const removerUltimaLinha = () => {
-    if (coords.length > 1) {
-      setCoords(coords.slice(0, -1));
+    if (coordenadas.length > 1) {
+      setCoordenadas(coordenadas.slice(0, -1));
     }
   };
 
-  const addRow = () => {
-    setCoords([...coords, { x: 0, y: 0 }]);
+  const adicionarLinha = () => {
+    setCoordenadas([...coordenadas, { x: 0, y: 0 }]);
   };
 
-  const handleCoordChange = (index, axis, value) => {
-    const updatedCoords = coords.map((coord, i) =>
-      i === index ? { ...coord, [axis]: Number(value) } : coord
+  const handleAlterarCoordenada = (index, eixo, valor) => {
+    const coordenadasAtualizadas = coordenadas.map((coord, i) =>
+      i === index ? { ...coord, [eixo]: Number(valor) } : coord
     );
-    setCoords(updatedCoords);
+    setCoordenadas(coordenadasAtualizadas);
   };
 
-  const minValue = Math.min(
-    Math.min(...coords.map((coord) => coord.x)),
-    Math.min(...coords.map((coord) => coord.y))
+  const valorMinimo = Math.min(
+    Math.min(...coordenadas.map((coord) => coord.x)),
+    Math.min(...coordenadas.map((coord) => coord.y))
   );
-  const maxValue = Math.max(
-    Math.max(...coords.map((coord) => coord.x)),
-    Math.max(...coords.map((coord) => coord.y))
+  const valorMaximo = Math.max(
+    Math.max(...coordenadas.map((coord) => coord.x)),
+    Math.max(...coordenadas.map((coord) => coord.y))
   );
 
   return (
@@ -103,14 +103,14 @@ export default function App() {
             </tr>
           </thead>
           <tbody>
-            {coords.map((coord, index) => (
+            {coordenadas.map((coord, index) => (
               <tr key={index}>
                 <td className="border border-gray-300 px-4 py-2">
                   <input
                     type="number"
                     value={coord.x}
                     onChange={(e) =>
-                      handleCoordChange(index, "x", e.target.value)
+                      handleAlterarCoordenada(index, "x", e.target.value)
                     }
                     className="w-full p-2 bg-gray-50 text-gray-900 rounded border border-gray-300"
                   />
@@ -120,7 +120,7 @@ export default function App() {
                     type="number"
                     value={coord.y}
                     onChange={(e) =>
-                      handleCoordChange(index, "y", e.target.value)
+                      handleAlterarCoordenada(index, "y", e.target.value)
                     }
                     className="w-full p-2 bg-gray-50 text-gray-900 rounded border border-gray-300"
                   />
@@ -131,13 +131,13 @@ export default function App() {
         </table>
         <div className="flex gap-4 mt-4">
           <button
-            onClick={addRow}
+            onClick={adicionarLinha}
             className="px-4 py-2 bg-green-500 hover:bg-green-600 rounded text-white shadow-md"
           >
             Adicionar Linha
           </button>
           <button
-            onClick={handleVerify}
+            onClick={handleVerificar}
             className="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded text-white shadow-md"
           >
             Verificar
@@ -152,8 +152,8 @@ export default function App() {
       </div>
 
       <div className="w-3/5 flex flex-col items-center justify-center border border-gray-300 bg-white p-4 rounded shadow-md">
-        <h3 className="text-lg font-semibold mb-2 text-center">{labelType}</h3>
-        <h3 className="text-lg mb-4 text-center">{law}</h3>
+        <h3 className="text-lg font-semibold mb-2 text-center">{tipoRotulo}</h3>
+        <h3 className="text-lg mb-4 text-center">{lei}</h3>
 
         <div
           style={{
@@ -167,8 +167,8 @@ export default function App() {
             data={{
               datasets: [
                 {
-                  label: `${labelType} Model`,
-                  data: data,
+                  label: `${tipoRotulo} Modelo`,
+                  data: dados,
                   borderColor: "rgba(75,192,192,1)",
                   backgroundColor: "rgba(75,192,192,0.2)",
                   tension: 0.2,
@@ -219,8 +219,8 @@ export default function App() {
               scales: {
                 x: {
                   type: "linear",
-                  min: minValue,
-                  max: maxValue,
+                  min: valorMinimo,
+                  max: valorMaximo,
                   grid: {
                     display: true,
                     color: "rgba(200, 200, 200, 0.2)",
@@ -247,8 +247,8 @@ export default function App() {
                 },
                 y: {
                   type: "linear",
-                  min: minValue,
-                  max: maxValue,
+                  min: valorMinimo,
+                  max: valorMaximo,
                   grid: {
                     display: true,
                     color: "rgba(200, 200, 200, 0.2)",
